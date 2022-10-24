5f2a39d2428ac7b5f40222dbef708e03859f0274
liviavalin <110104472+liviavalin@users.noreply.github.com>
2022-10-24 19:24:17 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -32,12 +32,32 @@ function cubo(){
 function mat(){
     let v = document.getElementById("valor").value;
     let j = document.getElementById("juros").value;
-    let t = document.getElementById("meses").value;
+    let t = document.getElementById("messes").value;
     let r = 0;
     for(let i=1; i<=t; i++){
         r = v * (1+(j/100));
         v = r;
-     }  
-    
+    }
     document.write("Resultado " + r);
 }
+function media(){
+    let n1 = document.getElementById("v1").value
+    let n2 = document.getElementById("v2").value
+    let n3 = document.getElementById("v3").value
+    let r = (Number(n1) + Number(n2) + Number(n3))/3;
+    document.getElementById("resultado").innerHTML = "Média : " + r;
+}
+function soma(){
+    let n1 = document.getElementById("v1").value
+    let n2 = document.getElementById("v2").value
+    let n3 = document.getElementById("v3").value
+    let r = (Number(n1) + Number(n2) + Number(n3));
+    document.getElementById("resultado").innerHTML = "Soma : " + r;
+}
+function subtração(){
+    let n1 = document.getElementById("v1").value
+    let n2 = document.getElementById("v2").value
+    let n3 = document.getElementById("v3").value
+    let r = (Number(n1) - Number(n2) - Number(n3));
+    document.getElementById("resultado").innerHTML = "Subtração : " + r;
+ }
